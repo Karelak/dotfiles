fbf0b2533c4ff5912804ca29f930ae16c9a92707
Kaarel Part <partkaarel@gmail.com>
2026-05-10 19:45:10 +0100

    Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1

--- readonly_Documents/readonly_PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/readonly_PowerShell/Microsoft.PowerShell_profile.ps1
@@ -96,6 +96,7 @@ Import-Module PSReadLine
 Import-Module PSFzf
 
 # PSFzf configuration
+$env:_PSFZF_FZF_DEFAULT_OPTS = "--cycle"
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 Set-PsFzfOption -TabExpansion
 
