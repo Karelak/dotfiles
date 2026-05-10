0986e75b7fd57de255fbd7b3ce602db4b5f06445
Kaarel Part <partkaarel@gmail.com>
2026-05-10 15:12:07 +0100

    Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1

--- readonly_Documents/readonly_PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/readonly_PowerShell/Microsoft.PowerShell_profile.ps1
@@ -26,21 +26,21 @@ Import-Module DockerCompletion
 # Shell completions
 Invoke-Expression (&chezmoi completion powershell | Out-String)
 Invoke-Expression (&surge completion powershell | Out-String)
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
-Invoke-Expression -Command $(gh completion -s powershell | Out-String)
+Invoke-Expression (& zoxide init powershell | Out-String)
+Invoke-Expression (&gh completion -s powershell | Out-String)
 
 Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
     param($wordToComplete, $commandAst, $cursorPosition)
-        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
-        $Local:word = $wordToComplete.Replace('"', '""')
-        $Local:ast = $commandAst.ToString().Replace('"', '""')
-        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
-            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-        }
+    [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+    $Local:word = $wordToComplete.Replace('"', '""')
+    $Local:ast = $commandAst.ToString().Replace('"', '""')
+    winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
 }
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
+    Import-Module "$ChocolateyProfile"
 }
 Import-Module scoop-completion
 . ([ScriptBlock]::Create((& scoop-search --hook | Out-String)))
