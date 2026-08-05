bb65af266facbf20b1049a17cb54a49e8292fbd9
Kaarel Part <partkaarel@gmail.com>
2026-08-05 12:15:54 +0100

    Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1

--- readonly_Documents/readonly_PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/readonly_PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,40 +1,36 @@
-
-# Core shell behaviour
 Import-Module PSReadLine
-# Import-Module PSFzf
-
-# PSFzf configuration
-# $env:FZF_DEFAULT_OPTS = "--cycle"
-# Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
-# Set-PsFzfOption -TabExpansion
 
 # Completions
-# Register-LazyCompleter -CommandName tailscale -Generator { tailscale completion powershell | Out-String }
-# Register-LazyCompleter -CommandName chezmoi -Generator { chezmoi completion powershell | Out-String }
-# Register-LazyCompleter -CommandName uv -Generator { uv --generate-shell-completion powershell | Out-String }
-# Register-LazyCompleter -CommandName uvx -Generator { uvx --generate-shell-completion powershell | Out-String }
-# Register-LazyCompleter -CommandName gh -Generator { gh completion -s powershell | Out-String }
-# Register-LazyCompleter -CommandName rustup -Generator { rustup completions powershell | Out-String }
-# Register-LazyCompleter -CommandName surge -Generator { surge completion powershell | Out-String }
-# Register-LazyCompleter -CommandName rclone -Generator { rclone completion powershell | Out-String }
-Import-Module ScoopCompletion
+Import-Module scoop-completion
 Import-Module DockerCompletion
+Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 
-# choco completions
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
-# winget is a special case because it doesn't have a PS completion script, but it can output completions directly also is really fucking slow but what can you do
-Register-LazyCompleter winget -NativeCompleter {
+# Generated completions
+tailscale completion powershell | Out-String | Invoke-Expression
+chezmoi completion powershell | Out-String | Invoke-Expression
+uv --generate-shell-completion powershell | Out-String | Invoke-Expression
+uvx --generate-shell-completion powershell | Out-String | Invoke-Expression
+gh completion -s powershell | Out-String | Invoke-Expression
+rustup completions powershell | Out-String | Invoke-Expression
+surge completion powershell | Out-String | Invoke-Expression
+rclone completion powershell | Out-String | Invoke-Expression
+
+# winget
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
   param($wordToComplete, $commandAst, $cursorPosition)
-  [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
-  $Local:word = $wordToComplete.Replace('"', '""')
-  $Local:ast = $commandAst.ToString().Replace('"', '""')
-  winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition |
-  ForEach-Object { [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_) }
-}
 
+  [Console]::InputEncoding = [Console]::OutputEncoding = [System.Text.UTF8Encoding]::new()
+
+  winget complete `
+    --word="$($wordToComplete.Replace('"', '""'))" `
+    --commandline "$($commandAst.ToString().Replace('"', '""'))" `
+    --position $cursorPosition |
+  ForEach-Object {
+    [System.Management.Automation.CompletionResult]::new(
+      $_, $_, 'ParameterValue', $_
+    )
+  }
+}
 # fast scoop search
 . ([ScriptBlock]::Create((& scoop-search --hook | Out-String)))
 
