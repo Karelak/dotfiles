5c6ddc746c6406436dddd8960ec2853474b4772b
Kaarel Part <partkaarel@gmail.com>
2026-08-05 12:17:19 +0100

    Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1

--- readonly_Documents/readonly_PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/readonly_PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,36 +1,5 @@
 Import-Module PSReadLine
 
-# Completions
-Import-Module scoop-completion
-Import-Module DockerCompletion
-Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-
-# Generated completions
-tailscale completion powershell | Out-String | Invoke-Expression
-chezmoi completion powershell | Out-String | Invoke-Expression
-uv --generate-shell-completion powershell | Out-String | Invoke-Expression
-uvx --generate-shell-completion powershell | Out-String | Invoke-Expression
-gh completion -s powershell | Out-String | Invoke-Expression
-rustup completions powershell | Out-String | Invoke-Expression
-surge completion powershell | Out-String | Invoke-Expression
-rclone completion powershell | Out-String | Invoke-Expression
-
-# winget
-Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
-  param($wordToComplete, $commandAst, $cursorPosition)
-
-  [Console]::InputEncoding = [Console]::OutputEncoding = [System.Text.UTF8Encoding]::new()
-
-  winget complete `
-    --word="$($wordToComplete.Replace('"', '""'))" `
-    --commandline "$($commandAst.ToString().Replace('"', '""'))" `
-    --position $cursorPosition |
-  ForEach-Object {
-    [System.Management.Automation.CompletionResult]::new(
-      $_, $_, 'ParameterValue', $_
-    )
-  }
-}
 # fast scoop search
 . ([ScriptBlock]::Create((& scoop-search --hook | Out-String)))
 
