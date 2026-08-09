67c1d7f396b6b229ce76d01ee82c7126344bc12c
Kaarel Part <partkaarel@gmail.com>
2026-08-09 19:37:14 +0100

    Add Documents/PowerShell/Microsoft.PowerShell_profile.ps1

---
+++ readonly_Documents/readonly_PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,28 @@
+Import-Module PSReadLine
+
+# PSReadLine preferences
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineOption -Colors @{ InlinePrediction = '#865d5d' }
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Chord 'Ctrl+RightArrow' -Function ForwardWord
+Set-PSReadLineOption -BellStyle None
+
+
+# fast scoop search
+. ([ScriptBlock]::Create((& scoop-search --hook | Out-String)))
+
+# Aliases
+Set-Alias c clear -Scope Global -Force
+
+# Prompt
+# Invoke-Expression (& oh-my-posh init pwsh --config "C:\Users\Kaarel\.config\oh-my-posh\zen.toml" | Out-String)
+# function Invoke-Starship-TransientFunction {
+#   &starship module character
+# }
+
+# Invoke-Expression (&starship init powershell)
+
+# Enable-TransientPrompt
